d5a9dc4ca9dc3a3abad4f5abd617e7e7ff12f8aa
NagariaHussain <hussainbhaitech@gmail.com>
2021-05-15 19:27:18 +0530

    feat: Question prompt text rendering

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import './style.css';
 import 'alpinejs';
+import MarkdownIt from 'markdown-it';
+
+var md = new MarkdownIt();
+
+window.fetchQuestion = () => {
+    return fetch('/questions/q1.json')
+    .then((r) => r.json())
+    .then((d) => {
+        return md.render(d.prompt_text)
+    });
+}
