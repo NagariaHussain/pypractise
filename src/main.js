9fdd35ed8d81132fee636c5f21cb1e7845aeb9bd
NagariaHussain <hussainbhaitech@gmail.com>
2021-05-15 23:40:12 +0530

    feat: Code Editor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import './style.css';
 import 'alpinejs';
 import MarkdownIt from 'markdown-it';
+import '../vendor/ace';
+
+// Configure code editor
+ace.config.set('basePath', '/vendor');
+const editor = ace.edit("code-editor");
+editor.setTheme("ace/theme/dracula");
+editor.session.setMode("ace/mode/python");
 
 const md = new MarkdownIt();
 
@@ -8,6 +15,7 @@ window.fetchQuestion = () => {
     return fetch('/questions/q1.json')
     .then((r) => r.json())
     .then((d) => {
-        return md.render(d.prompt_text)
+        editor.setValue(d.starter_code);
+        return md.render(d.prompt_text);
     });
 }
